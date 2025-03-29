5aa3a6cbfd7bce1dd8e5621f88a21cbe4a4a8261
sahil sharma <sahilsharma06478@gmail.com>
2025-03-30 00:12:47 +0530

    Navbar component created

---
+++ csgi/frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+// import Hero from "./components/Hero";
+// import OurInstitute from "./components/OurInstitute";
+// import OurInfrastructure from "./components/OurInfrastructure";
+// import PhotoGallery from "./components/PhotoGallery";
+// import MediaCoverage from "./components/MediaCoverage";
+// import VideoGallery from "./components/VideoGallery";
+// import CompaniesVisited from "./components/CompaniesVisited";
+// import PlacementHighlights from "./components/PlacementHighlights";
+// import Testimonials from "./components/Testimonials";
+// import Footer from "./components/Footer";
+
+export const Home =()=> {
+  return (
+    <div className="font-sans">
+      <Navbar />
+      {/* <Hero />
+      <OurInstitute />
+      <OurInfrastructure />
+      <PhotoGallery />
+      <MediaCoverage />
+      <VideoGallery />
+      <CompaniesVisited />
+      <PlacementHighlights />
+      <Testimonials />
+      <Footer /> */}
+    </div>
+  );
+}
+
