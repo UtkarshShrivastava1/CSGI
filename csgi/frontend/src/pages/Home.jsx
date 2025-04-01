3dbe258e8cbd32b435b9aa4c50a10d71a4b6b47f
DEVENDRA DILLIWAR <devendradilliwar30@gmail.com>
2025-04-01 13:57:04 +0530

    footer desgin

---
+++ csgi/frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Demo from '../components/Demo'
+
+const Home = () => {
+  return (
+    <>
+    <Footer /> 
+    <Demo />
+    </>
+  )
+}
+
+export default Home
